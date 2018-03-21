c46a2f64bafcb4b3d5a2c41369c787fa9106cd96
Matt Anderson <matt@mattbanderson.com>
2018-03-20 23:45:45 -0400

    Write output to CSV.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
+import csv
 import plistlib
+
+
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 
 if __name__ == '__main__':
     p1 = plistlib.readPlist('/Users/matt/code/eliz-texts/data/SAMSUNG-SM-G930V_20180318_184420/Messages.list')
-    for thread in p1['Threads']:
-        for item in thread['Items']:
-            if 'Content' in item and 'Number' in item and 'SendState' in item:
-                if item['SendState'] == 'Sent':
-                    print('{0} said: {1}'.format(item['Number'], item['Content']))
-                elif item['SendState'] == 'InBox':
-                    print('You replied to {0}: {1}'.format(item['Number'], item['Content']))
+    with open('/Users/matt/code/eliz-texts/output/texts.csv', 'w') as csvfile:
+        fieldnames = ['from', 'to', 'date', 'message']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        for thread in p1['Threads']:
+            for item in thread['Items']:
+                if 'Content' in item and 'Number' in item and 'SendState' in item:
+                    if item['SendState'] == 'Sent':
+                        writer.writerow({
+                            'from': item['Number'],
+                            'to': 'Elizabeth Anderson',
+                            'date': item['Time'].strftime(DATE_FORMAT),
+                            'message': item['Content']})
+                    elif item['SendState'] == 'InBox':
+                        writer.writerow({
+                            'from': 'Elizabeth Anderson',
+                            'to': item['Number'],
+                            'date': item['Time'].strftime(DATE_FORMAT),
+                            'message': item['Content']})
